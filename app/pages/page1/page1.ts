677c2a7ad3f7b90e9c4fd86db2a5eaab9329a27a
branp <servidor_team@hotmail.com>
2016-09-07 16:11:43 -0500

    dfdsf

---
+++ app/pages/page1/page1.ts
@@ -0,0 +1,56 @@
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import { Geolocation} from 'ionic-native';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/page1/page1.html'
+})
+
+export class Page1 {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  
+  constructor(public navCtrl: NavController) {
+  }
+  
+  ionViewLoaded(){
+         this.loadMap();
+  }      
+  
+  loadMap(){
+     Geolocation.getCurrentPosition().then((position) => {
+        //Puntos 
+        let punto1 =new google.maps.LatLng(-7.245252,-79.476013);
+        let punto2= new google.maps.LatLng(-7.30517 ,-79.480232);
+        let punto3= new google.maps.LatLng(-7.2500074,-79.4819764);
+        
+
+        //OpcionesMaps
+        let mapOptions = {
+         center: punto1,
+          zoom: 11,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        
+       
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+     
+     var marker1 = new google.maps.Marker({ position: punto1, map: this.map, title: 'Hello World!'});
+         marker1.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+    
+     var marker2 = new google.maps.Marker({ position: punto2, map: this.map, title: 'Hello World!'});
+         marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
+    
+
+     var marker3 = new google.maps.Marker({ position: punto3, map: this.map, title: 'Hello World!'});
+         marker3.setIcon('http://maps.google.com/mapfiles/ms/icons/yellow-dot.png');
+
+    }, (err) => {
+      console.log(err);
+    });
+    
+  }
+
+}
---
+++ app/pages/restaurant/restaurant.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the RestaurantPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/restaurant/restaurant.html',
+})
+export class Restaurant {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
